164ca77cc33080575d955613e74498d6bf5c94e4
Tollef Jørgensen <tollefj@gmail.com>
2018-04-10 10:15:39 +0200

    weather bg

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 import EthereumDashboard from "./EthereumDashboard";
+import Weather from './Weather';
 import Shortcuts from './Shortcuts'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Weather />
         <EthereumDashboard/>
         <Shortcuts/>
       </div>
